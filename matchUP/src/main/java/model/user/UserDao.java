7856563380dd217fb86394a17445c258c82dec51
poweiruty <rbxo0032@gmail.com>
2023-07-04 18:13:26 +0900

    cord sort

--- matchUP/src/main/java/model/user/UserDao.java
+++ matchUP/src/main/java/model/user/UserDao.java
@@ -1,7 +1,6 @@
 package model.user;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
