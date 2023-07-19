4d17d488f9a4de64ae9f72dcc4432dc66214bbbe
poweiruty <rbxo0032@gmail.com>
2023-07-19 21:00:02 +0900

    qweasd

--- matchUP/src/main/java/controller/corp/JobPostUpdateAction.java
+++ matchUP/src/main/java/controller/corp/JobPostUpdateAction.java
@@ -2,7 +2,7 @@ package controller.corp;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class JobPostUpdateAction
  */
-@WebServlet("/JobPostUpdateAction")
+//@WebServlet("/JobPostUpdateAction")
 public class JobPostUpdateAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
--- matchUP/src/main/java/controller/corp/SemibyMainAction.java
+++ matchUP/src/main/java/controller/corp/SemibyMainAction.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+//import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,7 +15,7 @@ import model.region.RegionDao;
 /**
  * Servlet implementation class SemibyMainAction
  */
-@WebServlet("/SemibyMainAction")
+//@WebServlet("/SemibyMainAction")
 public class SemibyMainAction extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
